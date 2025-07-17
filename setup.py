cab567ff2b9ece8a5558af0c1050386fd25dcfb8
Philipp Bleimund <bleimund.philipp@gmail.com>
2025-07-18 00:52:30 +0200

    added some to setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,15 @@ setup(
     version='0.0.1',
     description='A Python library to create simple Multy Layer Perceptron based neural networks.',
     author='Philipp Bleimund',
-    install_requires=[],
+    install_requires=['numpy'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
     test_suite='tests',
+    extras_require={
+        "dev": [
+            "pytest",
+            "keras",
+            "tensorflow"
+        ],
+    },
 )
