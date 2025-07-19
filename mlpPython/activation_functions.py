e13deab6cadded4641eb733b23b3e81d00bad319
Philipp Bleimund <bleimund.philipp@gmail.com>
2025-07-19 21:18:30 +0200

    added relu

--- mlpPython/activation_functions.py
+++ mlpPython/activation_functions.py
@@ -6,14 +6,18 @@ def get_activation_function(activation_function):
         return lambda z: sigmoid(z)
     elif activation_function == "softmax":
         return lambda z: softmax(z)
+    elif activation_function == "relu":
+        return lambda z: relu(z)
     return lambda z: z
 
 
 def get_activation_function_abl(activation_function):
     if activation_function == "sigmoid":
         return lambda z: sigmoid_abl(z)
-    if activation_function == "softmax":
+    elif activation_function == "softmax":
         return lambda z: 1
+    elif activation_function == "relu":
+        return lambda z: relu_abl(z)
 
     return lambda z: z
 
@@ -24,6 +28,14 @@ def sigmoid(z):
 
 def sigmoid_abl(z):
     return sigmoid(z) * (1 - sigmoid(z))
+
+
+def relu(z):
+    return np.maximum(z, 0)
+
+
+def relu_abl(z):
+    return 1. * (z > 0)
 
 
 def softmax(z):
