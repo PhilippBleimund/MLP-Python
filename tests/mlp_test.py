835c59fbf0586a3538f2884bc6237b785e91f4bf
Philipp Bleimund <bleimund.philipp@gmail.com>
2025-07-21 21:34:22 +0200

    adapted to new data preperation

--- tests/mlp_test.py
+++ tests/mlp_test.py
@@ -1,26 +1,21 @@
-from mlpPython.Model import Model
-from mlpPython.Layer import InputLayer, PerceptronLayer, PredictionLayer
+from mlpPython import Model, InputLayer, PerceptronLayer, PredictionLayer
 
-from keras.datasets import cifar10
-from keras.utils import to_categorical
 import numpy as np
+
+from .load_cifar_10 import load_cifar_10_data
 
 
 def test_core():
-    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+    train_data, train_filenames, train_labels, test_data, test_filenames, test_labels, label_names = \
+        load_cifar_10_data()
 
     cifar_classes = ['airplane', 'automobile', 'bird', 'cat',
                      'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-    # Transform label indices to one-hot encoded vectors
-
-    y_train = to_categorical(y_train, num_classes=10)
-    Y_test = to_categorical(y_test, num_classes=10)
-
     # Transform images from (32,32,3) to 3072-dimensional vectors (32*32*3)
 
-    X_train = np.reshape(X_train, (50000, 3072))
-    X_test = np.reshape(X_test, (10000, 3072))
+    X_train = np.reshape(train_data, (50000, 3072))
+    X_test = np.reshape(test_data, (10000, 3072))
     X_train = X_train.astype('float32')
     X_test = X_test.astype('float32')
 
@@ -36,12 +31,14 @@ def test_core():
     model.add(PerceptronLayer(256, "relu"))
     model.add(PredictionLayer(10, cifar_classes))
     model.assemble_model()
-    model.train_model(X_train, y_train, 200)
+    model.train_model(X_train, train_labels, 1000)
+
+    print("now testing")
 
     # check acuracy
     num_correct = 0
     for i in range(len(X_test)):
-        num_correct += model(X_test[i]) == cifar_classes[y_test[i, 0]]
+        num_correct += model(X_test[i]) == cifar_classes[test_labels[i]]
     print(f"accuracy: {(num_correct/len(X_test))*100}%")
 
     print(model(X_test[0]))
